af80f3e89b7488b9f370ffb71cb7f18e76fa1502
rachel3834 <rstreet@lco.global>
2018-01-24 17:36:04 +1300

    Adding guidelines for the data challenge

--- tutorial/management/commands/populate_site.py
+++ tutorial/management/commands/populate_site.py
@@ -15,14 +15,22 @@ class Command(BaseCommand):
     args = ''
     help = ''
     
-    def _create_site_page(self):
-        file_list = glob.glob('tutorial/static/site/site_*html')
-        file_list = file_list + glob.glob('tutorial/static/tutorial/site_*html')
+    def add_arguments(self, parser):
+        parser.add_argument('filename', nargs='+', type=str)
+        
+    def _create_site_page(self,*args, **options):
+        filename = options['filename'][0]
+        if filename == 'ALL':
+            file_list = glob.glob('tutorial/static/site/site_*html')
+            file_list = file_list + glob.glob('tutorial/static/tutorial/site_*html')
+        else:
+            file_list = glob.glob('tutorial/static/tutorial/'+filename)
         for f in file_list:
+            print('Parsing '+f)
             file_lines = open(f,'r').readlines()
             params = ingest_functions.parse_article(file_lines)
             page, created = SitePage.objects.get_or_create(**params)
     
     def handle(self,*args, **options):
-        self._create_site_page()
+        self._create_site_page(*args, **options)
 
