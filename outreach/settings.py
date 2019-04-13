0a0be8ddbec8f2bac630076f29ed572986c1035a
rachel3834 <rstreet@lco.global>
2019-04-12 19:55:46 -0700

    Updated settings

--- outreach/settings.py
+++ outreach/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q2ut$jip_k2bx7fx#y8roj7wv78u%3au1)mm8wg0wguyh^vgr7'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join('/var/www/microlensingonline/', 'sqlite.db'),
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      #  'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
